286357b355dcf1fa134ef79ef340ade8e385bc63
Jason John <jasonjohn@Jasons-MacBook-Pro.local>
2018-10-01 13:56:16 -0500

    added mongodb to persist data

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
+mongoose.connect('mongodb+srv://jason:7sDhraDXBW1bKu1e@mycluster-htwsm.mongodb.net/node-angular?retryWrites=true')
+  .then(() => {
+    console.log('connected to database');
+  })
+  .catch(() => {
+    console.log('connection failed');
+  })
 app.use(bodyParser.json());
 
+//7sDhraDXBW1bKu1e
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.setHeader('Access-Control-Allow-Headers',
-   'Origin, X-Requested-Width, Content-Type, Accept');
-   res.setHeader('Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-   next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-       id: 'fse32',
-       title: 'First server side post',
-       content: 'This is coming from the server'
-    },
-    {
-      id: 'ouhr345',
-      title: 'Second server side post again',
-      content: 'This is coming from the server'
-   },
-   {
-    id: 'oiuh8493',
-    title: 'Third server side post',
-    content: 'This is coming from the server. Woohoo!'
- },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-  });
+  //return all entries
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: documents
+      });
+    });
+});
+
+app.delete('/api/posts/:id',(req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: 'Post deleted'
+    });
+  })
 });
 
 module.exports = app;
---
+++ backend/models/post.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+
+//create a blueprint / schema
+const postSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true}
+});
+
+//convert schema to model then export it
+module.exports = mongoose.model('Post', postSchema);
