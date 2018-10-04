e75b64c001d2aed1cddad5106c193ca2ff93477b
Jason John <jasonjohn@Jasons-MacBook-Pro.local>
2018-10-04 17:06:23 -0500

    add ability to upload image and validate mime type

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -16,6 +17,8 @@ import { Post } from '../post.model';
    private postId: string;
    post: Post;
    isLoading = false;
+   form: FormGroup;
+   imagePreview: string;
 
 
    constructor(public postsService: PostsService, public route: ActivatedRoute) {
@@ -23,6 +26,11 @@ import { Post } from '../post.model';
    }
 
    ngOnInit() {
+     this.form = new FormGroup({
+       'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+       'content': new FormControl(null, {validators: [Validators.required]}),
+       'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+     });
      this.route.paramMap.subscribe((paramMap: ParamMap) => {
        if (paramMap.has('postId')) {
           this.mode = 'edit';
@@ -31,6 +39,7 @@ import { Post } from '../post.model';
           this.postsService.getPost(this.postId).subscribe(postData => {
             this.isLoading = false;
             this.post = {id: postData._id, title: postData.title, content: postData.content};
+            this.form.setValue({'title': this.post.title, 'content': this.post.content});
           });
        } else {
          this.mode = 'create';
@@ -39,16 +48,27 @@ import { Post } from '../post.model';
   });
 }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = <string>reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
